d8f1a13bf65ddcd2c19eba13dca1e489c34db98d
hyzhao <houyazhao@gmail.com>
2023-06-06 22:39:42 +0800

    github action

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,6 +5,7 @@ export default defineUserConfig({
   title: 'web前端面试 - 面试官系列',
   description:
     'web前端面试,vue面试题,react面试题,js面试题,大厂面试题,阿里面试题,京东面试题',
+  base: '/interview/',
   theme: defaultTheme({
     sidebar: [
       {
