fd5f84030b51073acb22274eb6f110daac1aa114
hyzhao <houyazhao@gmail.com>
2023-06-07 11:00:27 +0800

    update config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,11 +2,13 @@ import { defineUserConfig, defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: 'web前端面试 - 面试官系列',
+  title: 'web前端面试',
   description:
     'web前端面试,vue面试题,react面试题,js面试题,大厂面试题,阿里面试题,京东面试题',
-  base: '/interview/',
+  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
+  base: process.env.NODE_ENV === 'development' ? '/' : '/interview/',
   theme: defaultTheme({
+    logo: '/images/logo.png',
     sidebar: [
       {
         text: '简介',
